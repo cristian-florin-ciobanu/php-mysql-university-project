6f5d698aad79bc2c8d9d636e3ed80d82eecbe573
Cristian-Florin Ciobanu <cristian-florin-ciobanu@proton.me>
2026-06-14 20:06:33 +0300

    front page news section (not styled)

--- footer.php
+++ footer.php
@@ -9,13 +9,13 @@ function queryRole($role) {
     if ($result->num_rows > 0)
         while ($row = $result->fetch_assoc()) echo "<b>".$role."</b>" . "<br>" . $row["First Name"]." ".$row["Last Name"] . "<br>";
     else echo $error_message;
+    echo "<br>";
 }
 ?>
 
 <style type="text/css"><?php include 'css/footer.css'; ?></style>
 
 <div id="footer">
-
     <div id="footer-roles">
         <h3>University</h3>
         <div id="footer-roles-list">
@@ -40,11 +40,9 @@ function queryRole($role) {
             </div>
         </div>
     </div>
-
     <div class="footer-contact">
         <h3>Contact</h3>
         <a class="footer-contact-link" href="tel:+40-257-280702">✆ +40-257-280702</a><br>
         <a class="footer-contact-link" href="mailto:rector@uni.ro">✉ rector@uni.ro</a>
     </div>
-
 </div>
--- index.php
+++ index.php
@@ -13,6 +13,30 @@
     <h3 id="news-header">News</h3>
     <p id="news-subheader">News and communications</p>
     <hr>
+    <?php
+        require 'connect.php';
+        $error_message = "Could not find any news or events to display.";
+        $result = $connection->query("SELECT `ID`, `Name`, `Text` FROM `news`");
+        
+        if ($result->num_rows <= 0) {
+            echo "There are no news or events.";
+        } else {
+            $news_count = 0;
+            echo "<div id=\"news\">";
+            while ($row = $result->fetch_assoc()) {
+                echo "<div class=\"news-element\"><h3>".$row["Name"]."</h3><br><p>".$row["Text"]."</p></div>";
+                
+                $news_count++;
+                if ($news_count >= 4) {
+                    echo "<a id=\"read-more-news\" href=\"news.php\">Read More</a>";
+                    break;
+                }
+            }
+            echo "</div>";
+        }
+    ?>
+        
+    </div>
 
     <?php include 'footer.php'; ?>
 </body>
