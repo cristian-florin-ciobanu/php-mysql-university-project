19f5b8fecfd99ce096519abe76db05dc05da678c
Cristian-Florin Ciobanu <cristian-florin-ciobanu@proton.me>
2026-06-14 19:16:08 +0300

    starting the news section

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 </head>
 <body>
     <style type="text/css"><?php include 'css/index.css'; ?></style>
+
     <?php include 'header.php'; ?>
+
     <h1>University of Arad</h1>
+    
+    <h3 id="news-header">News</h3>
+    <p id="news-subheader">News and communications</p>
+    <hr>
+
     <?php include 'footer.php'; ?>
 </body>
